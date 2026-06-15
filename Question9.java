b63f68dfbc80610664cab4dab80a9b7596e05a11
kawin <kawin>
2026-06-15 22:12:27 +0530

    Add Questions 8 to 15

---
+++ Question9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input an integer: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("The sum of the digits is: " + sum);
+    }
+}
