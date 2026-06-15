b26c4dc2859cc07f0f6f92b22630f3fe9d1b3dc7
kawin <kawin>
2026-06-15 22:07:24 +0530

    Add Question13 even or odd program

---
+++ Question13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Question13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
